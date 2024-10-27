37de75818ca55c778af4cca50e8f238984703aa9
UrielDonadel <sba24222@student.cct.ie>
2024-10-27 11:50:12 +0000

    CA1
    Created main class CA1 to load customer data, validate entries, calculate discounts, and save/display results.

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -28,6 +28,26 @@ public class CA1 {
             String totalPurchaseStr = customerData.get(i + 2);
             String classStr = customerData.get (i + 3);
             String lastPurchaseYearStr = customerData.get (i + 4);
+            
+            //Create and validate customer
+            Customer customer = new Customer(firstName, secondName, totalPurchaseStr, classStr, lastPurchaseYearStr);
+            if (customer.isValid()) {
+                //calculate final value with discount
+                double finalValue = DiscountCalculator, calculateDiscountedValue(customer);
+                
+                //write result to output file
+                FileHandler.writeToFile(customer, finalValue);
+                
+                //display result in console for verification
+                System.out.println("%s %s - Final Value: %.2f, Discount Applied: %.0f%%%n", customer.getFirstName(), customer.getSecondName(), finalValue, customer.getDiscountRate() * 100);
+            }
+            else{
+                //Show error message if any validation fails
+                System.out.println("Invalid data format for customer starting with: " + customer.getFirstName());
+            }
+        }
+            System.out.println("Processing complete. Discount calculated, saved, and displayed in console. ");
+            }
         }
         
     }
